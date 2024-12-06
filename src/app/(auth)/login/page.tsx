cf857f64a1872d3558925686bdb8f2a97d7b8107
@designervenkat <murty.av@gmail.com>
2024-12-06 03:25:14 +0000

    route groups and private folders

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div className='grid place-content-center h-screen'>Login Page</div>
+  )
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SignUpPage() {
+  return (
+    <div className='grid place-content-center h-screen'>Signup Page</div>
+  )
+}
