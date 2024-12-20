fe652e8dbf15fe650ba10343d38fe5b31c90a9d7
@designervenkat <murty.av@gmail.com>
2024-12-20 02:34:32 +0000

    basic updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,18 @@
-import Image from "next/image";
-import code from "./../../public/code-unsplash.jpg"
-import conway from "./../../public/conway-unsplash.jpg"
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <>
-      <Image
-     
-        src={code}
-        alt="image"
-        sizes="(max-width: 768px) 100vw, 33vw"
-    />
-    
-    <Image
-      
-        src={conway}
-        alt="image"
-        quality={80}
-        priority
-
-      />
-
- 
-    </>
-  );
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <Image
+                    className="dark:invert"
+                    src="/next.svg"
+                    alt="Next.js logo"
+                    width={180}
+                    height={38}
+                    priority
+                />
+            </main>
+        </div>
+    )
 }
